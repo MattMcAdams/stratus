21562c1ae7f4069c1fbe3e514eba6622afe09214
MattMcAdams <mattmcadams@outlook.com>
2019-11-21 16:55:30 -0600

    try to set up tests

--- test/stratus.spec.js
+++ test/stratus.spec.js
@@ -1,14 +1,13 @@
 var path = require('path');
 var sassTrue = require('sass-true');
 
-var sassFile = path.join(__dirname, '_stratus.spec.scss');
+var sassFile = path.join(__dirname, 'stratus.spec.scss');
 sassTrue.runSass(
-  {
-    file: sassFile,
-  },
-  {
-    sass: require('sass'),
-    describe,
-    it,
-  }
-);
+    {
+    file: sassFile
+    },
+    {
+      describe,
+      it
+    }
+  );
