29bbee936e1abdf687a0f09a4ad306e480564328
MattMcAdams <mattmcadams@outlook.com>
2019-12-18 14:06:19 -0600

    update sass-true

--- test/stratus.spec.js
+++ test/stratus.spec.js
@@ -1,14 +1,17 @@
 var path = require('path');
 var sassTrue = require('sass-true');
 
+var path = require('path');
+var sassTrue = require('sass-true');
+
 var sassFile = path.join(__dirname, 'stratus.spec.scss');
 sassTrue.runSass(
-    {
+  {
     file: sassFile,
-    includePaths: ['node_modules'],
-    },
-    {
-      describe,
-      it
-    }
-  );
+  },
+  {
+    sass: require('dart-sass'),
+    describe,
+    it,
+  }
+);
